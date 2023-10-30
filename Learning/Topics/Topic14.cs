c6d5028505613a56815970412ae8e061a6282e98
Skrecior <skretowskaaleksandra@gmail.com>
2023-10-30 19:00:12 +0100

    Topic14 fixed

--- Learning/Topics/Topic14.cs
+++ Learning/Topics/Topic14.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Learning.Topics
+﻿namespace Learning.Topics
 {
-    internal class Topic14
+    public class Topic14
     {
+        public static void Run()
+        {
+            Console.WriteLine("1 - Americano\n2 - Espresso\n3 - Cappuccino");
+            Console.WriteLine("Provide the number of the coffee you want to choose:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Americano");
+                    break;
+                case 2:
+                    Console.WriteLine("Espresso");
+                    break;
+                case 3:
+                    Console.WriteLine("Cappuccino");
+                    break;
+                default:
+                    Console.WriteLine("Unknown");
+                    break;
+            }
+        }
     }
 }
